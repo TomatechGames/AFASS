f718e19c06c909e9227ddefeb607218db3861b19
TomatechGames <theludwiggamer@gmail.com>
2023-09-03 17:13:29 +0100

    Added a task method to append assets to the Savable Resource Manager

--- Runtime/SavableResourceManager.cs
+++ Runtime/SavableResourceManager.cs
@@ -21,6 +21,23 @@ namespace Tomatech.AFASS
             return null;
         }
 
+        public static async Task AppendSavableAssets(string[] newKeys, bool withLogs = false)
+        {
+            var toLoad = newKeys.Except(loadedAssets.Keys);
+            if (withLogs)
+                Debug.Log("loading " + toLoad.Count() + " key(s)");
+            foreach (var key in toLoad)
+            {
+                var assetHandle = Addressables.LoadAssetAsync<Object>(key);
+                loadedAssets.Add(key, assetHandle);
+            }
+            if (withLogs)
+                Debug.Log("waiting for assets to load...");
+            await Task.WhenAll(loadedAssets.Values.Select(a => a.Task));
+            if (withLogs)
+                Debug.Log("loading complete with " + loadedAssets.Count + " loaded object" + (loadedAssets.Count == 1 ? "" : "s"));
+        }
+
         public static async Task PreloadSaveableAssets(string[] newKeys, bool withLogs=false)
         {
             var toUnload = loadedAssets.Keys.Except(newKeys);
